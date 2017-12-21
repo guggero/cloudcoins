006805eb924a6386e0aaeddb2dad3bd6087f78ff
Robin Lack <lack@puzzle.ch>
2017-12-21 17:20:42 +0100

    fixed some typos

--- README.md
+++ README.md
@@ -8,21 +8,21 @@ The project is in a very early stage, so this is work in progress.
 
 ## development
 
-### Preparing Projects
-1. fork and clone repo
-1. import into intellij
-1. run maven task `install` on projects `cloudcoins-frontend` `cloudcoins-backend`
+### Preparing projects
+1. Fork and clone repo
+1. Import into IntelliJ
+1. Run maven task `install` on projects `cloudcoins-frontend` `cloudcoins-backend`
 
-### Setting up local Application server 
+### Setting up local application server 
 1. Download WildFly [10.1.0-Final](http://download.jboss.org/wildfly/10.1.0.Final/wildfly-10.1.0.Final.zip) and extract content
 1. Copy files to local WildFly. You can use the script: copy-files-to-jboss.sh
-   1. usage: `./copy-files-to-jboss.sh /absolute/installation/path/wildfly-10.1.0.Final`
-1. Add Jboss server in Intellij
-   1. on top right dropdown -> edit configurations
-   1. add a new one with `+`
-   1. choose local Jboss
-   1. specify Jboss directory
-   1. In deployment tab, add Artifacts with `+`, choose exploded versions
+   1. Usage: `./copy-files-to-jboss.sh /absolute/installation/path/wildfly-10.1.0.Final`
+1. Add JBoss server in IntelliJ
+   1. On top right dropdown -> edit configurations
+   1. Add a new one with `+`
+   1. Choose local JBoss
+   1. Specify JBoss directory
+   1. In deployment tab, add artifacts with `+`, choose exploded versions
 
 ### start local database
 
--- copy-files-to-jboss.sh
+++ copy-files-to-jboss.sh
@@ -3,32 +3,32 @@
 
 if [ $# -eq 0 ]
   then
-    echo "No arguments supplied... Please provide jboss home folder"
-    echo "example:"
+    echo "No arguments supplied... Please provide JBoss home folder"
+    echo "Example:"
     echo "      ./copy-files-to-jboss.sh /tmp/wildfly-10.1.0.Final"
 fi
 
 if [ $# -eq 1 ]
   then
     JBOSS_HOME_FOLDER=$1
-    echo "using $1 as jboss home"
+    echo "Using $1 as JBoss home"
     JBOSS_CONFIG_FOLDER=$JBOSS_HOME_FOLDER/standalone/configuration
 
     mkdir -p $JBOSS_HOME_FOLDER/modules/org/postgresql/jdbc/main
-    echo "created path org/postgresql/jdbc/main in $1/modules"
+    echo "Created path org/postgresql/jdbc/main in $1/modules"
 
     MAIN_FOLDER=$JBOSS_HOME_FOLDER/modules/org/postgresql/jdbc/main
 
     yes | cp -f standalone.xml $JBOSS_CONFIG_FOLDER
-    echo "override $JBOSS_CONFIG_FOLDER/standalone.xml"
+    echo "Override $JBOSS_CONFIG_FOLDER/standalone.xml"
 
     yes | cp -f docker/wildfly/configuration/application.keystore $JBOSS_CONFIG_FOLDER
     yes | cp -f docker/wildfly/configuration/application-users.properties $JBOSS_CONFIG_FOLDER
     yes | cp -f docker/wildfly/configuration/application-roles.properties $JBOSS_CONFIG_FOLDER
-    echo "copied application.keystore, application-users.properties, application-roles.properties to $JBOSS_CONFIG_FOLDER"
+    echo "Copied application.keystore, application-users.properties, application-roles.properties to $JBOSS_CONFIG_FOLDER"
 
     yes | cp -f docker/wildfly/jdbc-driver/module.xml $MAIN_FOLDER
     yes | cp -f docker/wildfly/jdbc-driver/postgresql-9.4.1211.jar $MAIN_FOLDER
-    echo "copied module.xml and postgresql-9.4.1211.jar to $MAIN_FOLDER"
-    echo "files successfully copied!"
+    echo "Copied module.xml and postgresql-9.4.1211.jar to $MAIN_FOLDER"
+    echo "Files successfully copied!"
 fi
